3fd081a3fc881281cdd7e588cc5b15867339bbe3
Maksym Shylov <maks20060606@gmail.com>
2025-07-24 21:27:09 +0200

    update requirements.txt

--- bot_start.py
+++ bot_start.py
@@ -2,6 +2,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 from dotenv import load_dotenv, find_dotenv
 import os
 
+
 #import from handlers
 from handlers.start_command import start
 from handlers.stop_command import stop
--- handlers/stop_command.py
+++ handlers/stop_command.py
@@ -1,18 +1,19 @@
 # Command /stop
 from telegram import Update
-from telegram.ext import ContextTypes
-async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+import os
+async def stop(update: Update, context):
     user = update.effective_user
     print(f"/stop and logs deleted {user.id}, {user.name}")
-
+    user = update.effective_user
+    user_id = user.id
     # clear user_data
     context.user_data.clear()
 
-    # Delete the user log file
-    # filename = f"user_{user_id}.txt"
-    # folder = "user_logs"
-    # filepath = os.path.join(folder, filename)
-    #
-    # if os.path.exists(filepath):
-    #     os.remove(filepath)
-    # await update.message.reply_text(f"Bye {update.message.from_user.first_name}!")
+    #Delete the user log file
+    filename = f"user_{user_id}.txt"
+    folder = "user_logs"
+    filepath = os.path.join(folder, filename)
+
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    await update.message.reply_text(f"Bye {update.message.from_user.first_name}!")
