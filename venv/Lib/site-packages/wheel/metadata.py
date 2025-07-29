ac64a71177989f2ae2b789cf441a2f598bee3c56
Maksym Shylov <maks20060606@gmail.com>
2025-07-29 22:21:19 +0200

    debug

--- venv/Lib/site-packages/wheel/metadata.py
+++ venv/Lib/site-packages/wheel/metadata.py
@@ -1,28 +1,101 @@
 """
 Tools for converting old- to new-style metadata.
 """
+
 from __future__ import annotations
 
+import functools
+import itertools
 import os.path
+import re
 import textwrap
 from email.message import Message
 from email.parser import Parser
-from typing import Iterator
+from typing import Generator, Iterable, Iterator, Literal
 
-from pkg_resources import Requirement, safe_extra, split_sections
+from .vendored.packaging.requirements import Requirement
+
+
+def _nonblank(str: str) -> bool | Literal[""]:
+    return str and not str.startswith("#")
+
+
+@functools.singledispatch
+def yield_lines(iterable: Iterable[str]) -> Iterator[str]:
+    r"""
+    Yield valid lines of a string or iterable.
+    >>> list(yield_lines(''))
+    []
+    >>> list(yield_lines(['foo', 'bar']))
+    ['foo', 'bar']
+    >>> list(yield_lines('foo\nbar'))
+    ['foo', 'bar']
+    >>> list(yield_lines('\nfoo\n#bar\nbaz #comment'))
+    ['foo', 'baz #comment']
+    >>> list(yield_lines(['foo\nbar', 'baz', 'bing\n\n\n']))
+    ['foo', 'bar', 'baz', 'bing']
+    """
+    return itertools.chain.from_iterable(map(yield_lines, iterable))
+
+
+@yield_lines.register(str)
+def _(text: str) -> Iterator[str]:
+    return filter(_nonblank, map(str.strip, text.splitlines()))
+
+
+def split_sections(
+    s: str | Iterator[str],
+) -> Generator[tuple[str | None, list[str]], None, None]:
+    """Split a string or iterable thereof into (section, content) pairs
+    Each ``section`` is a stripped version of the section header ("[section]")
+    and each ``content`` is a list of stripped lines excluding blank lines and
+    comment-only lines.  If there are any such lines before the first section
+    header, they're returned in a first ``section`` of ``None``.
+    """
+    section = None
+    content: list[str] = []
+    for line in yield_lines(s):
+        if line.startswith("["):
+            if line.endswith("]"):
+                if section or content:
+                    yield section, content
+                section = line[1:-1].strip()
+                content = []
+            else:
+                raise ValueError("Invalid section heading", line)
+        else:
+            content.append(line)
+
+    # wrap up last segment
+    yield section, content
+
+
+def safe_extra(extra: str) -> str:
+    """Convert an arbitrary string to a standard 'extra' name
+    Any runs of non-alphanumeric characters are replaced with a single '_',
+    and the result is always lowercased.
+    """
+    return re.sub("[^A-Za-z0-9.-]+", "_", extra).lower()
+
+
+def safe_name(name: str) -> str:
+    """Convert an arbitrary string to a standard distribution name
+    Any runs of non-alphanumeric/. characters are replaced with a single '-'.
+    """
+    return re.sub("[^A-Za-z0-9.]+", "-", name)
 
 
 def requires_to_requires_dist(requirement: Requirement) -> str:
     """Return the version specifier for a requirement in PEP 345/566 fashion."""
-    if getattr(requirement, "url", None):
+    if requirement.url:
         return " @ " + requirement.url
 
-    requires_dist = []
-    for op, ver in requirement.specs:
-        requires_dist.append(op + ver)
+    requires_dist: list[str] = []
+    for spec in requirement.specifier:
+        requires_dist.append(spec.operator + spec.version)
 
     if requires_dist:
-        return " (" + ",".join(sorted(requires_dist)) + ")"
+        return " " + ",".join(sorted(requires_dist))
     else:
         return ""
 
@@ -30,17 +103,17 @@ def requires_to_requires_dist(requirement: Requirement) -> str:
 def convert_requirements(requirements: list[str]) -> Iterator[str]:
     """Yield Requires-Dist: strings for parsed requirements strings."""
     for req in requirements:
-        parsed_requirement = Requirement.parse(req)
+        parsed_requirement = Requirement(req)
         spec = requires_to_requires_dist(parsed_requirement)
-        extras = ",".join(sorted(parsed_requirement.extras))
+        extras = ",".join(sorted(safe_extra(e) for e in parsed_requirement.extras))
         if extras:
             extras = f"[{extras}]"
 
-        yield parsed_requirement.project_name + extras + spec
+        yield safe_name(parsed_requirement.name) + extras + spec
 
 
 def generate_requirements(
-    extras_require: dict[str, list[str]]
+    extras_require: dict[str | None, list[str]],
 ) -> Iterator[tuple[str, str]]:
     """
     Convert requirements from a setup()-style dictionary to
@@ -60,13 +133,14 @@ def generate_requirements(
             yield "Provides-Extra", extra
             if condition:
                 condition = "(" + condition + ") and "
-            condition += "extra == '%s'" % extra
+            condition += f"extra == '{extra}'"
 
         if condition:
             condition = " ; " + condition
 
         for new_req in convert_requirements(depends):
-            yield "Requires-Dist", new_req + condition
+            canonical_req = str(Requirement(new_req + condition))
+            yield "Requires-Dist", canonical_req
 
 
 def pkginfo_to_metadata(egg_info_path: str, pkginfo_path: str) -> Message:
@@ -82,7 +156,7 @@ def pkginfo_to_metadata(egg_info_path: str, pkginfo_path: str) -> Message:
     del pkg_info["Requires-Dist"]
     requires_path = os.path.join(egg_info_path, "requires.txt")
     if os.path.exists(requires_path):
-        with open(requires_path) as requires_file:
+        with open(requires_path, encoding="utf-8") as requires_file:
             requires = requires_file.read()
 
         parsed_requirements = sorted(split_sections(requires), key=lambda x: x[0] or "")
