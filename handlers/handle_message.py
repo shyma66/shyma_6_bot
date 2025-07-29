90964adbd0a17801f73650b1f143402357a09d15
Maksym Shylov <maks20060606@gmail.com>
2025-07-29 17:46:56 +0200

    update handle code

--- handlers/handle_message.py
+++ handlers/handle_message.py
@@ -1,19 +1,33 @@
 # message from user
 from telegram import Update
 from telegram.ext import ContextTypes
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     user_id = user.id
-    text = update.message.text
-    print(f"message from: {user.id}, {user.name} ")
+    is_active = context.application.bot_data.get(user_id, True)
+    if not is_active:
+        return
+    user_message = update.message.text
+    print(f"message from: {user_id}, {user.name} , message: {user_message} ")
     # Path to user file (eg: user_12345678.txt)
     filename = f"user_{user_id}.txt"
     folder = "user_logs"
     os.makedirs(folder, exist_ok=True)
     filepath = os.path.join(folder, filename)
-
     # Save the message to a file
     with open(filepath, "a", encoding="utf-8") as f:
-        f.write(text + "\n")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+        f.write(user_message + "\n")
+
+
+
+
+
+
+
+
+
